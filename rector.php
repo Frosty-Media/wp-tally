48d3ae4635506c00b4890571a778e18036c2d63a
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-05-15 08:40:29 -0700

    Make to to ignore First Class Callables.

--- rector.php
+++ rector.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Php81\Rector\Array_\FirstClassCallableRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -10,6 +11,10 @@ return RectorConfig::configure()
     ])
     // uncomment to reach your current PHP version
     ->withPhpSets(php83: true)
+    ->withSkip([
+        __DIR__ . '/includes/*',
+        FirstClassCallableRector::class,
+    ])
     ->withTypeCoverageLevel(0)
     ->withDeadCodeLevel(0)
     ->withCodeQualityLevel(0);
