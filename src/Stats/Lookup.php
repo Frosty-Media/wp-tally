cabbfad7d7471fca3261e71515b9b82610bef5ff
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-05-23 09:37:39 -0700

    Change upgrade to admin_post action, and add notice.

--- src/Stats/Lookup.php
+++ src/Stats/Lookup.php
@@ -6,10 +6,16 @@ namespace FrostyMedia\WpTally\Stats;
 
 use Symfony\Component\HttpFoundation\Request;
 use TheFrosty\WpUtilities\Plugin\AbstractSingletonProvider;
+use function admin_url;
 use function filter_var;
 use function get_plugin_data;
 use function sanitize_text_field;
+use function sprintf;
 use function update_option;
+use function wp_get_referer;
+use function wp_kses_post;
+use function wp_safe_redirect;
+use function wpautop;
 use const FILTER_FLAG_IPV4;
 use const FILTER_FLAG_IPV6;
 use const FILTER_VALIDATE_IP;
@@ -23,6 +29,7 @@ class Lookup extends AbstractSingletonProvider
     public const string OPTION = '_wptally_stats';
     public const string VIEW_API = 'api';
     public const string VIEW_SHORTCODE = 'shortcode';
+    protected const string ACTION = 'wptally_upgrade';
     protected const string TOTAL_COUNT = 'total_count';
     protected const string USERS = 'users';
     protected const string USERS_COUNT = 'count';
@@ -34,7 +41,8 @@ class Lookup extends AbstractSingletonProvider
      */
     public function addHooks(): void
     {
-        $this->addAction('load-index.php', [$this, 'maybeUpgradeOption']);
+        $this->addAction('admin_notices', [$this, 'adminNotice']);
+        $this->addAction('admin_post_' . self::ACTION, [$this, 'upgradeOption']);
     }
 
     /**
@@ -94,9 +102,26 @@ class Lookup extends AbstractSingletonProvider
     }
 
     /**
+     * Admin notice to run our option update.
+     */
+    protected function adminNotice(): void
+    {
+        $option = self::getOption();
+        $current_version = get_plugin_data($this->getPlugin()->getFile(), translate: false)['Version'];
+        $db_version = $option[self::VERSION] ?? null;
+        if ($db_version === null || $db_version < $current_version) {
+            $message = sprintf(
+                'WP Tally requires an update. Please <a href="%s">click here</a>.',
+                esc_url(add_query_arg('action', self::ACTION, admin_url('admin-post.php')))
+            );
+            echo wp_kses_post(sprintf('<div class="notice">%s</div>', wpautop($message)));
+        }
+    }
+
+    /**
      * Maybe trigger an option upgrade.
      */
-    protected function maybeUpgradeOption(): void
+    protected function upgradeOption(): never
     {
         $option = self::getOption();
         $current_version = get_plugin_data($this->getPlugin()->getFile(), translate: false)['Version'];
@@ -106,11 +131,11 @@ class Lookup extends AbstractSingletonProvider
         if ($db_version === null) {
             $option[self::VERSION] = '2.0.0';
             self::updateOption($option);
-            $this->maybeUpgradeOption();
+            $this->upgradeOption();
         }
         // Version 2.2.0 (DB options structure change).
         if ($db_version < '2.1.1') {
-            $users = $option[self::USERS];
+            $users = $option[self::USERS] ?? [];
             $_users = [];
             foreach ($users as $username => $data) {
                 $_users[$username][self::TOTAL_COUNT] = $data[self::USERS_COUNT];
@@ -124,6 +149,9 @@ class Lookup extends AbstractSingletonProvider
             $option[self::VERSION] = $current_version;
             self::updateOption($option);
         }
+
+        wp_safe_redirect(wp_get_referer() ? wp_get_referer() : admin_url());
+        exit;
     }
 
     /**
