22aad072f012fd71613d18227c5f2cf0f472f55d
Austin Passy <367897+thefrosty@users.noreply.github.com>
2025-05-23 18:36:22 -0700

    Add filters to Rate Limit limiter and period.

--- src/Route/Limiter.php
+++ src/Route/Limiter.php
@@ -6,8 +6,10 @@ namespace FrostyMedia\WpTally\Route;
 
 use WP_Error;
 use WP_Http;
+use function apply_filters;
 use function filter_var;
 use function FrostyMedia\WpTally\getIpAddress;
+use function get_called_class;
 use function header;
 use function headers_sent;
 use function status_header;
@@ -33,6 +35,8 @@ trait Limiter
      */
     public function rateLimiter(int $limit = 100, int $period = 60): WP_Error|int
     {
+        $limit = (int)apply_filters('frosty_media_wp_tally_rate_limit_limit', $limit, static::class);
+        $period = (int)apply_filters('frosty_media_wp_tally_rate_limit_period', $period, static::class);
         $data = get_option('_tally_rate_limit', []);
         $error = new WP_Error();
 
